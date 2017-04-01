4a03127fdff6db41959cd2e3959fc0dbcf2e9568
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-04-01 20:13:37 +0100

    Prepared a test list for the intialisation command.

--- spec/initialisation_spec.rb
+++ spec/initialisation_spec.rb
@@ -1,5 +1,25 @@
 describe 'Initialisation command' do
-  it 'adds 1 + 1 correctly' do
-    expect(1 + 1).to eq 2
+  describe 'I 0 0' do
+    it 'creates a 0 x 0 image with all pixels painted white'
+  end
+
+  describe 'I 1 0' do
+    it 'creates a 1 x 0 image with all pixels painted white'
+  end
+
+  describe 'I 0 1' do
+    it 'creates a 0 x 1 image with all pixels painted white'
+  end
+
+  describe 'I 5 2' do
+    it 'creates a 5 x 2 image with all pixels painted white'
+  end
+
+  describe 'I 4 7' do
+    it 'creates a 4 x 7 image with all pixels painted white'
+  end
+
+  describe 'I 8 8' do
+    it 'creates a 8 x 8 image with all pixels painted white'
   end
 end
