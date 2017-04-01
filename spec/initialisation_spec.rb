27ee83bf700444bf039fd86e0d3d5a4308c8b012
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-04-01 20:43:09 +0100

    Refactored - moved duplicate setup to helper methods.

--- spec/initialisation_spec.rb
+++ spec/initialisation_spec.rb
@@ -10,31 +10,30 @@ describe 'Initialisation command' do
       @image.create(width: width, height: height)
     end
   end
-  
+
+  let(:image) { double(:image) }
+  let(:bitmap_editor) { BitmapEditor.new(image) }
   describe 'I 0 0' do
     it 'creates a 0 x 0 image with all pixels painted white' do
-      image = double(:image)
       expect(image).to receive(:create).with(width: 0, height: 0)
 
-      BitmapEditor.new(image).run 'I 0 0'
+      bitmap_editor.run('I 0 0')
     end
   end
 
   describe 'I 1 0' do
     it 'creates a 1 x 0 image with all pixels painted white' do
-      image = double(:image)
       expect(image).to receive(:create).with(width: 1, height: 0)
 
-      BitmapEditor.new(image).run 'I 1 0'
+      bitmap_editor.run('I 1 0')
     end
   end
 
   describe 'I 0 1' do
     it 'creates a 0 x 1 image with all pixels painted white' do
-      image = double(:image)
       expect(image).to receive(:create).with(width: 0, height: 1)
 
-      BitmapEditor.new(image).run('I 0 1')
+      bitmap_editor.run('I 0 1')
     end
   end
 
