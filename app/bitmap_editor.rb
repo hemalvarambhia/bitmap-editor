9b4431be348db9626982f5fd5db08bd1c1562838
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-04-04 13:31:26 +0100

    Refactor - inlined the parsing, and extracted the width and heights in 1 go.

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -6,9 +6,8 @@ class BitmapEditor
   def run(command)
     case command[0]
     when 'I'
-      width = command.split(' ')[1].to_i.abs
-      height = command.split(' ')[2].to_i.abs
-      @image.create(width: width, height: height)
+      _, width, height = command.split(' ')
+      @image.create(width: width.to_i.abs, height: height.to_i.abs)
     when 'L'
       y = command.split(' ')[2].to_i
       @image.paint_pixel(coordinate: OpenStruct.new(x: 1, y: y), colour: 'A')
