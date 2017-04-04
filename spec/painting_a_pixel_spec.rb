3116ec7dd09dcea286b7c8ac91c769c87c980dfb
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-04-04 12:49:14 +0100

    Added a test list for painting a pixel on the image.

---
+++ spec/painting_a_pixel_spec.rb
@@ -0,0 +1,17 @@
+describe 'Painting a pixel' do
+  describe 'L 1 1 A' do
+    it "paints co-ordinate (1, 1) the colour 'A'"
+  end
+
+  describe 'L 1 2 A' do
+    it "paints co-ordinate (1, 2) the colour 'A'"
+  end
+
+  describe 'L 3 5 A' do
+    it "paints co-ordinate (3, 5) the colour 'A'"
+  end
+
+  describe 'L 6 7 X' do
+    it "paints co-ordinate (6, 7) the colour 'X'"
+  end
+end
