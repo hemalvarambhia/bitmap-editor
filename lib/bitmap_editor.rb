9b3e00c6b18ce2164e60ed5a59355465a5a832bc
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-04-06 12:50:18 +0100

    Refactor - removed duplication in the logic of parsing the command.

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -4,14 +4,16 @@ class BitmapEditor
   end
 
   def run(command)
-    case command[0]
+    type, *args = command.split(' ')
+    case type
     when 'I'
-      _, width, height = command.split(' ')
-      @image.create(width: width.to_i.abs, height: height.to_i.abs)
+      width = args[0].to_i.abs
+      height = args[1].to_i.abs
+      @image.create(width: width, height: height)
     when 'L'
-      x = command.split(' ')[1].to_i
-      y = command.split(' ')[2].to_i
-      colour = command.split(' ')[3]
+      x = args[0].to_i
+      y = args[1].to_i
+      colour = args[2]
       @image.paint_pixel(coordinate: OpenStruct.new(x: x, y: y), colour: colour)
     end  
   end
