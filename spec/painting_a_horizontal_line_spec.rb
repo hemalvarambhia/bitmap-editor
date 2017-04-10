47168f5065c10d85d3e20b5cc8b50f56cc68b2ce
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-04-10 12:50:16 +0100

    Added a test list for painting a horizontal line a given colour.

---
+++ spec/painting_a_horizontal_line_spec.rb
@@ -0,0 +1,15 @@
+describe 'H 1 1 1 I' do
+  it 'paints a pixel at (1, 1) the colour I'
+end
+
+describe 'H 1 3 1 I' do
+  it "paints a horizontal line from (1, 1) to (3, 1) the colour I"
+end
+
+describe 'H 3 5 1 I' do
+  it "paints a horizontal line from (3, 1) to (5, 1) the colour I"
+end
+
+describe 'H 3 5 7 I' do
+  it "paints a horizontal line from (3, 7) to (5, 7) the colour I"
+end
