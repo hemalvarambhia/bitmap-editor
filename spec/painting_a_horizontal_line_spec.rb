7f159a1ebc1fca26dc8f407b708db06ea9b2da7e
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-04-13 06:28:53 +0100

    Triangulation - we can now paint a horizontal line from (x_1, 1) to (x_2, 1) the colour 'I'.

--- spec/painting_a_horizontal_line_spec.rb
+++ spec/painting_a_horizontal_line_spec.rb
@@ -26,7 +26,16 @@ describe 'H 1 3 1 I' do
 end
 
 describe 'H 3 5 1 I' do
-  it "paints a horizontal line from (3, 1) to (5, 1) the colour I"
+  it "paints a horizontal line from (3, 1) to (5, 1) the colour I" do
+    image = double(:image)
+    expect(image).to receive(:paint_horizontal_line).with(
+                       from: OpenStruct.new(x: 3, y: 1),
+                       to: OpenStruct.new(x: 5, y: 1),
+                       colour: 'I'
+                     )
+
+    BitmapEditor.new(image).run('H 3 5 1 I')
+  end
 end
 
 describe 'H 3 5 7 I' do
