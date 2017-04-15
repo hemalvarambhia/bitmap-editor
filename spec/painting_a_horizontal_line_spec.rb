f3d0135f7a1edd0fe643bcf4fabd95ba088d614a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-04-15 01:16:31 +0100

    Refactor - moved the duplicate set up and initialisation code to set up helper methods.

--- spec/painting_a_horizontal_line_spec.rb
+++ spec/painting_a_horizontal_line_spec.rb
@@ -1,53 +1,52 @@
 require 'ostruct'
 require 'bitmap_editor'
 describe 'Painting a horizontal line' do
+  let(:image) { double(:image) }
+  let(:bitmap_editor) { BitmapEditor.new(image) }
+  
   describe 'H 1 1 1 I' do
     it "paints a pixel at (1, 1) the colour 'I'" do
-      image = double(:image)
       expect(image).to(receive(:paint_horizontal_line).with(
                         from: OpenStruct.new(x: 1, y: 1),
                         to: OpenStruct.new(x: 1, y: 1),
                         colour: 'I'))
 
-      BitmapEditor.new(image).run('H 1 1 1 I')
+      bitmap_editor.run('H 1 1 1 I')
     end
   end
 
   describe 'H 1 3 1 I' do
     it "paints a horizontal line from (1, 1) to (3, 1) the colour 'I'" do
-      image = double(:image)
       expect(image).to receive(:paint_horizontal_line).with(
                          from: OpenStruct.new(x: 1, y: 1),
                          to: OpenStruct.new(x: 3, y: 1),
                          colour: 'I'
                        )
 
-      BitmapEditor.new(image).run('H 1 3 1 I')
+      bitmap_editor.run('H 1 3 1 I')
     end
   end
 
   describe 'H 3 5 1 I' do
     it "paints a horizontal line from (3, 1) to (5, 1) the colour 'I'" do
-      image = double(:image)
       expect(image).to receive(:paint_horizontal_line).with(
                          from: OpenStruct.new(x: 3, y: 1),
                          to: OpenStruct.new(x: 5, y: 1),
                          colour: 'I'
                        )
 
-      BitmapEditor.new(image).run('H 3 5 1 I')
+      bitmap_editor.run('H 3 5 1 I')
     end
   end
 
   describe 'H 3 5 7 I' do
     it "paints a horizontal line from (3, 7) to (5, 7) the colour 'I'" do
-      image = double(:image)
       expect(image).to(receive(:paint_horizontal_line).with(
                         from: OpenStruct.new(x: 3, y: 7),
                         to: OpenStruct.new(x: 5, y: 7),
                         colour: 'I'))
 
-      BitmapEditor.new(image).run('H 3 5 7 I')
+      bitmap_editor.run('H 3 5 7 I')
     end
   end
 end
