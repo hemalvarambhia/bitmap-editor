9c2bb794215ab77d02fd7441ed309a73f2c7de5a
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-04-19 13:14:13 +0100

    Refactor - removed the duplicate set up of image and editor to helper methods.

--- spec/painting_a_vertical_line_spec.rb
+++ spec/painting_a_vertical_line_spec.rb
@@ -2,33 +2,33 @@ require 'ostruct'
 require 'spec_helper'
 
 describe 'Painting a vertical line' do
+  let(:image) { double(:image) }
+  let(:bitmap_editor) { BitmapEditor.new(image) }
+
   describe 'V 1 1 1 X' do
     it "paints a pixel at (1, 1) the colour 'X'" do
-      image = double(:image)
       line = [ OpenStruct.new(x: 1, y: 1) ]
       expect(image).to receive(:paint_line).with(line, 'X')
 
-      BitmapEditor.new(image).run('V 1 1 1 X')
+      bitmap_editor.run('V 1 1 1 X')
     end
   end
 
   describe 'V 2 1 1 X' do
     it "paints a pixel at (2, 1) the colour 'X'" do
-      image = double(:image)
       line = [ OpenStruct.new(x: 2, y: 1) ]
       expect(image).to receive(:paint_line).with(line, 'X')
 
-      BitmapEditor.new(image).run('V 2 1 1 X')
+      bitmap_editor.run('V 2 1 1 X')
     end
   end
 
   describe 'V 1 3 3 X' do
     it "paints a vertical line from (1, 3) the colour 'X'" do
-      image = double(:image)
       line = [ OpenStruct.new(x: 1, y: 3) ]
       expect(image).to receive(:paint_line).with(line, 'X')
 
-      BitmapEditor.new(image).run('V 1 3 3 X')
+      bitmap_editor.run('V 1 3 3 X')
     end
   end
 end
