c09de828aec7a7def69c148abc7bcf4e828e29b0
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-05-05 13:07:07 +0100

    Added an example to demonstrate the editor interprets the command case insensitively.

--- spec/displaying_the_canvas_spec.rb
+++ spec/displaying_the_canvas_spec.rb
@@ -10,4 +10,14 @@ describe 'Displaying the image' do
       BitmapEditor.new(image, output_stream).run 'S'
     end
   end
+
+  describe 's' do
+    it 'displays the image to an output' do
+      image = double :image
+      output_stream = double :output_stream
+      expect(image).to receive(:display).with output_stream
+
+      BitmapEditor.new(image, output_stream).run 's'
+    end
+  end
 end
