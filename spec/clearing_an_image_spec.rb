ca360c319e4429ec422311a03d6e5e6b41dfaf4a
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-05-08 13:00:03 +0100

    Refactor - moved duplicate arrange code to set up helper methods.

--- spec/clearing_an_image_spec.rb
+++ spec/clearing_an_image_spec.rb
@@ -1,22 +1,20 @@
 require 'spec_helper'
 
 describe 'Clearing an image' do
+  let(:image) { double(:image) }
+  let(:bitmap_editor) { BitmapEditor.new(image, nil) }
   describe 'C' do
     it 'clears the image' do
-      image = double(:image)
       expect(image).to receive(:clear)
       
-      bitmap_editor = BitmapEditor.new(image, nil)
       bitmap_editor.run 'C'
     end
   end
 
   describe 'c' do
     it 'clears the image' do
-      image = double(:image)
       expect(image).to receive(:clear)
       
-      bitmap_editor = BitmapEditor.new(image, nil)
       bitmap_editor.run 'c'
     end
   end
