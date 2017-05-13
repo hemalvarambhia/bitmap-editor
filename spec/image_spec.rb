540ef86386b20ef053d05f6df11c6175b36ce956
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-05-13 16:06:08 +0100

    Triangulation - we can now create a white image of any dimension.

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -2,16 +2,16 @@ class Image
   attr_reader :width, :height, :pixels
   def create(dimensions)
     @width = dimensions[:width]
-    @height = 1
-    @pixels = Array.new(1) { 'O' * @width }
+    @height = dimensions[:height]
+    @pixels = Array.new(@height) { 'O' * @width }
     @width = @pixels[0].size
   end
 end
 
 describe Image do
+  let(:image) { Image.new }
   describe 'initialising a 1 x 1 image' do
     it 'has a width of 1 px and a height of 1px' do
-      image = Image.new
       image.create(width: 1, height: 1)
 
       expect(image.width).to eq 1
@@ -19,7 +19,6 @@ describe Image do
     end
 
     it 'is coloured white' do
-      image = Image.new
       image.create(width: 1, height: 1)
 
       expect(image.pixels).to eq ['O']
@@ -28,7 +27,6 @@ describe Image do
 
   describe 'initialising a 2 x 1 image' do
     it 'has a width of 2px and a height of 1px' do
-      image = Image.new
       image.create(width: 2, height: 1)
 
       expect(image.width).to eq 2
@@ -36,10 +34,24 @@ describe Image do
     end
 
     it 'is coloured white' do
-      image = Image.new
       image.create(width: 2, height: 1)
 
       expect(image.pixels).to eq [ 'OO' ]
+    end
+  end
+
+  describe 'initialising a 2 x 3 image' do
+    it 'has a width of 2px and a height of 3px' do
+      image.create(width: 2, height: 3)
+
+      expect(image.width).to eq 2
+      expect(image.height).to eq 3
+    end
+
+    it 'is painted white' do
+      image.create(width: 2, height: 3)
+
+      expect(image.pixels).to eq ['OO', 'OO', 'OO']
     end
   end
 end
