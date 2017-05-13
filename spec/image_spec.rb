c0007bf076f2a034f61a9ca6491801cacf68f086
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-05-13 15:58:00 +0100

    Triangulation - we can create a white image of any width as long as the height is one.

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,9 +1,10 @@
 class Image
   attr_reader :width, :height, :pixels
   def create(dimensions)
-    @width = 1
+    @width = dimensions[:width]
     @height = 1
-    @pixels = ['O']
+    @pixels = Array.new(1) { 'O' * @width }
+    @width = @pixels[0].size
   end
 end
 
@@ -22,6 +23,23 @@ describe Image do
       image.create(width: 1, height: 1)
 
       expect(image.pixels).to eq ['O']
+    end
+  end
+
+  describe 'initialising a 2 x 1 image' do
+    it 'has a width of 2px and a height of 1px' do
+      image = Image.new
+      image.create(width: 2, height: 1)
+
+      expect(image.width).to eq 2
+      expect(image.height).to eq 1
+    end
+
+    it 'is coloured white' do
+      image = Image.new
+      image.create(width: 2, height: 1)
+
+      expect(image.pixels).to eq [ 'OO' ]
     end
   end
 end
