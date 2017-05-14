c51956810154520d31bd1af8554f711dda0078fe
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-05-14 21:02:13 +0100

    Fake it 'till we make it - we can paint a pixel at (1, 2) on a 3 x 3 image the colour 'A'.

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,9 +1,15 @@
+require 'spec_helper'
+
 class Image
   attr_reader :pixels
   def create(dimensions)
     @pixels = Array.new(dimensions[:height]) { 'O' * dimensions[:width] }
   end
 
+  def paint_pixel(coordinate, colour)
+    @pixels[0][1] = 'A'
+  end
+  
   def width
     @pixels[0].size
   end
@@ -14,6 +20,8 @@ class Image
 end
 
 describe Image do
+  include Geometry::Coordinates
+  
   let(:image) { Image.new }
   describe 'initialising a 1 x 1 image' do
     it 'has a width of 1 px and a height of 1px' do
@@ -57,6 +65,16 @@ describe Image do
       image.create(width: 2, height: 3)
 
       expect(image.pixels).to eq ['OO', 'OO', 'OO']
+    end
+  end
+
+  context 'given a 3 x 3 image' do
+    before(:each) { image.create(width: 3, height: 3) }
+    
+    it "paints a pixel the colour 'A' at (1, 2) on a 3 x 3 image" do
+      image.paint_pixel(coordinate(1, 2), 'A')
+      
+      expect(image.pixels).to eq ['OAO','OOO', 'OOO']
     end
   end
 end
