405eb0dbbf1587ba9df4b9339b9e259553b4c7a0
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-05-31 06:08:39 +0100

    Added an integration test to verify we can paint a pixel on a 5 x 6 white image.

--- spec/integration/bitmap_editor_spec.rb
+++ spec/integration/bitmap_editor_spec.rb
@@ -9,4 +9,15 @@ describe BitmapEditor do
     expected = "OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO"
     expect(image.to_s).to eq expected
   end
+
+  it 'paints a pixel on a 5px x 6px image' do
+    image = Image.new
+    editor = BitmapEditor.new(image, nil)
+    editor.run 'I 5 6'
+
+    editor.run 'L 2 3 A'
+    
+    expected = "OOOOO\nOOOOO\nOAOOO\nOOOOO\nOOOOO\nOOOOO"
+    expect(image.to_s).to eq expected
+  end
 end
