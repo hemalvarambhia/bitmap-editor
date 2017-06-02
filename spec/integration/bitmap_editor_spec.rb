10e975972a679f1b34529fff9028af0ebdd89034
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-06-02 06:34:53 +0100

    Added an integration test for painting a horizontal line; removed duplicate arrange code to set up helpers.

--- spec/integration/bitmap_editor_spec.rb
+++ spec/integration/bitmap_editor_spec.rb
@@ -1,23 +1,31 @@
 require 'spec_helper'
 require 'image'
 describe BitmapEditor do
+  let(:image) { Image.new }
+  let(:editor) { BitmapEditor.new(image, nil) }
+  
   it 'generates a 5px x 6px white image' do
-    image = Image.new
-      
-    BitmapEditor.new(image, nil).run 'I 5 6'
+    editor.run 'I 5 6'
 
     expected = "OOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO"
     expect(image.to_s).to eq expected
   end
 
   it 'paints a pixel on a 5px x 6px image' do
-    image = Image.new
-    editor = BitmapEditor.new(image, nil)
     editor.run 'I 5 6'
 
     editor.run 'L 2 3 A'
     
     expected = "OOOOO\nOOOOO\nOAOOO\nOOOOO\nOOOOO\nOOOOO"
+    expect(image.to_s).to eq expected
+  end
+
+  it 'paints a horizontal line on a 5px x 6px white image' do
+    editor.run 'I 5 6'
+
+    editor.run 'H 3 5 2 Z'
+
+    expected = "OOOOO\nOOZZZ\nOOOOO\nOOOOO\nOOOOO\nOOOOO"
     expect(image.to_s).to eq expected
   end
 end
