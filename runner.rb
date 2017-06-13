dcc8e204ab6921a22cc3dafd7639314887387831
Hemal Varambhia <hvarambhia@bookingbug.com>
2017-06-13 15:18:12 +0100

    The runner file is ready to use; added a test file of commands taken from the README.

--- runner.rb
+++ runner.rb
@@ -1,3 +1,11 @@
-require './app/bitmap_editor'
-
-BitmapEditor.new.run ARGV.first
+$LOAD_PATH << 'lib'
+require 'geometry/coordinates'
+require 'image'
+require 'display'
+require 'bitmap_editor'
+image = Image.new
+display = Display.new(STDOUT)
+File.open(ARGV.first).each do |command|
+  display.show command
+  BitmapEditor.new(image, display).run command
+end
